9b97c9c62ccff7169c45fbf6c6a1c6e594c116e8
irvsanz <irvsanz@gmail.com>
2026-02-16 20:45:15 -0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WellbeingApp from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WellbeingApp />
-  </React.StrictMode>,
-)
