ae447c1f87f350fb118e2a0647485e2127af3458
irvsanz <irvsanz@gmail.com>
2026-02-16 20:41:41 -0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WellbeingApp from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WellbeingApp />
+  </React.StrictMode>,
+)
